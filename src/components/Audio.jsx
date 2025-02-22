bd77f130265b1ee26ec18b47cddfed2893a53a26
Thalles <thalleslopesm@gmail.com>
2025-02-22 12:09:14 -0300

    Final touch

--- src/components/Audio.jsx
+++ src/components/Audio.jsx
@@ -5,28 +5,26 @@ export const Audio = () => {
   const audioEnabled = useStore((state) => state.audioEnabled)
 
   return (
-    <>
-      {audioEnabled && (
-        <>
-          <group position={[0, 0, 0]}>
-            <PositionalAudio
-              autoplay
-              loop
-              url="/sounds/waves.mp3"
-              distance={50}
-            />
-          </group>
+    audioEnabled && (
+      <>
+        <group position={[0, 0, 0]}>
+          <PositionalAudio
+            autoplay
+            loop
+            url="/sounds/waves.mp3"
+            distance={50}
+          />
+        </group>
 
-          <group position={[-65, 35, -55]}>
-            <PositionalAudio
-              autoplay
-              loop
-              url="/sounds/birds.mp3"
-              distance={30}
-            />
-          </group>
-        </>
-      )}
-    </>
+        <group position={[-65, 35, -55]}>
+          <PositionalAudio
+            autoplay
+            loop
+            url="/sounds/birds.mp3"
+            distance={30}
+          />
+        </group>
+      </>
+    )
   )
 }
